924f22e2b38c08c6777ce9063ece863309f19cb6
Adam Mitchell <adamstuartmitchell@gmail.com>
2017-11-18 13:49:32 +0000

    Initial work on FeatureExtract class. Added framing functionality and partial windowing functionality

---
+++ main.py
@@ -0,0 +1,37 @@
+# Import futures
+from __future__ import division
+
+# Import from standard lib
+from collections import OrderedDict
+
+# Import 3rd party libs
+import numpy
+from scipy.io.wavfile import read
+
+
+class FeatureExtract(object):
+    def __init__(self, data, mmap=False):
+        self.fs, self.data = read(data, mmap=mmap)
+
+    def frame(self, length=25, overlap=0.5):
+        self.frames = OrderedDict()
+
+        self.length = self.fs / (1000 / length)
+        self.overlap = self.length * overlap
+        self.pos = 0
+
+        while self.pos < len(self.data):
+            self.frames[int(self.pos)] = self.data[int(self.pos):int(self.pos+self.length)]
+            self.pos = self.pos+self.overlap
+
+    def window(self, frame, type='hamming'):
+        self.windows = {'blackman': numpy.blackman, 'hanning': numpy.hanning,
+                        'hamming':  numpy.hamming,  'bartlett': numpy.bartlett
+                       }
+
+        self.window['hamming'](self.length)
+        
+
+f = FeatureExtract('today.wav')
+f.frame()
+f.window()
