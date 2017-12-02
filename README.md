450cfd90daa235de9e26c36050a1efec9a437796
Adam Mitchell <adamstuartmitchell@gmail.com>
2017-12-02 01:10:00 +0000

    Updated Readme. Compartmentalised code, ready to apply filterbank to fft'd signal.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# MFCC Automatic Speech Recognition Implementation
+# MFCC Automatic Speech Recognition Algorithm Implementation
 
-A python implementation of the Mel Frequency Cepstral Coefficient algorithm for the detection of phonemes in continuous speech.
+A Python implementation of the Mel Frequency Cepstral Coefficient algorithm for the detection of phonemes in continuous speech.
+
+## Method
+
+1. Extract audio signal and sampling frequency from .wav file
+2. Frame signal
+3. Apply window function to frame
+4. Calculate DFT of frame
+5. Calculate power spectral density estimate for each DFT bin
+6. Apply Mel Frequency filter bank to signal
+7. Calculate logarithm of energy in each filter
+8. Take DCT of each filter
+9. Voila!
--- main.py
+++ main.py
@@ -4,19 +4,43 @@
 # Email:  adamstuartmitchell@gmail.com
 
 import filterbank as fb
-import dft, time
+import dft
 
 from scipy.io.wavfile import read
 from matplotlib import pyplot as plt
 
+'''
+Current arguments required for class:
+
+wav file (will provide audio data and sampling frequency)
+npoints for fft
+filterbank lf and hf end points
+filterbank nfilters
+window function
+
+Must add:
+
+padding function for frames - pad end with 0s to reach frame length
+feature extract class
+lexicon
+language model
+feature vector database
+'''
+
 
 if __name__ == '__main__':
-    start = time.time()
     p, c = fb.get_filterbank(22050, 512, 80, 8000, 26)
     fs, data = read('1k.wav')
-
     f = data[0:400]
     m = dft.fft(f)
-    print 'Took: {0}s'.format(time.time()-start)
-    plt.plot(m)
-    plt.show()
+
+    for key, val in c.iteritems():
+        fnum = key
+        lbin = int(val['lbin'])
+        hbin = int(val['hbin'])
+
+        for idx, c in enumerate(val['coeffs']):
+            print 'Filter: {0:2}  Bin: {1:2}  Coeff: {2:8}  Mag: {3:8}'.format(fnum, lbin+idx, c, m[lbin+idx])
+
+    #plt.plot(m)
+    #plt.show()
