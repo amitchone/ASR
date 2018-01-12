c955b0ff3d0c1e4f88bb3ad7781fd0c6845f71fa
Adam Mitchell <adamstuartmitchell@gmail.com>
2018-01-12 02:57:08 +0000

    Adding mfcc.py; simplified implementation for clarity and proof

---
+++ mfcc.py
@@ -0,0 +1,48 @@
+# mfcc.py
+# Compute MFCCs of a given WAV file
+# Author: Adam Mitchell
+# Email:  adamstuartmitchell@gmail.com
+
+
+import numpy
+from scipy.io.wavfile import read
+
+
+def open_file(f):
+    '''
+    return sampling frequency (fs) and time series data (data) of .wav file (f)
+    '''
+    fs, data = read(f)
+
+    return fs, data
+
+
+def frame(data, fs, length=0.025, shift=0.010):
+    '''
+    return array of arrays where each array is a framed segment of data (data)
+    length of frames is given as sampling frequency (fs) * length (in ms)
+    shift between frames is given as sampling frequency (fs) * shift (in ms)
+    '''
+    length = int(round(fs * length))
+    shift = int(round(fs * shift))
+    frames = list()
+
+    for i in range(0, len(data), shift):
+        frame = data[i:i+length]
+
+        if len(frame) != length:
+            frame = numpy.append(frame, [0] * (length-len(frame)))
+
+        frames.append(frame)
+
+    return numpy.array(frames)
+
+
+def window():
+    pass
+
+
+if __name__ == '__main__':
+    fs, data = open_file('wavs/one-adam-1.wav')
+    frames = frame(data, fs)
+    print frames.shape
