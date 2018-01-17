97f6159d265b6d72a18f17459baddefdedc9b244
Adam Mitchell <adamstuartmitchell@gmail.com>
2018-01-17 17:32:39 +0000

    Tracking db dir and added template training data db

---
+++ db/mfcc_training_data_empty.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.6.31, for osx10.8 (x86_64)
+--
+-- Host: localhost    Database: mfcc_training_data
+-- ------------------------------------------------------
+-- Server version	5.6.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `mfcc_training_data`
+--
+
+DROP TABLE IF EXISTS `mfcc_training_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mfcc_training_data` (
+  `idmfcc_training_data` int(11) NOT NULL,
+  `filename` mediumtext,
+  `filepath` text,
+  `num_value` varchar(45) DEFAULT NULL,
+  `word_value` varchar(45) DEFAULT NULL,
+  `vector` longtext,
+  `time_added` datetime DEFAULT NULL,
+  PRIMARY KEY (`idmfcc_training_data`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mfcc_training_data`
+--
+
+LOCK TABLES `mfcc_training_data` WRITE;
+/*!40000 ALTER TABLE `mfcc_training_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mfcc_training_data` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-17 17:31:44
