1b0a6fb47593b5f79e7e0b134498d157a43ea705
Adam Mitchell <adamstuartmitchell@gmail.com>
2018-01-17 16:51:45 +0000

    Tracking db.py

---
+++ db.py
@@ -0,0 +1,6 @@
+# db.py
+# Handle MySQL database connection and queries
+# Author: Adam Mitchell
+# Email:  adamstuartmitchell@gmail.com
+
+import MySQLdb
--- install/setup.sh
+++ install/setup.sh
@@ -1,9 +1,11 @@
-echo Installing requirements for MFCC/DTW feature extraction...
+echo Installing required packages for MFCC/DTW feature extraction...
 echo
 cd ..
 virtualenv venv
 . venv/bin/activate
-brew install portaudio 
+brew install portaudio
 pip install -r install/requirements.txt
+pip install -U MySQL-python==1.2.5
+sudo ln -s /usr/local/mysql/lib/libmysqlclient.18.dylib /usr/local/lib/libmysqlclient.18.dylib
 echo
 echo Installation complete!
