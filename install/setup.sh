2eda77a443110bf689c7e106ce7eb73009731f23
Adam Mitchell <adamstuartmitchell@gmail.com>
2018-01-19 16:36:22 +0000

    brew install portaudio sooner in installation script to avoid progressing if not installed

--- install/setup.sh
+++ install/setup.sh
@@ -1,10 +1,10 @@
 echo Installing required packages for MFCC/DTW feature extraction...
 echo
+brew install portaudio
 pip install virtualenv
 cd ..
 virtualenv venv
 . venv/bin/activate
-brew install portaudio
 pip install -r install/requirements.txt
 sudo ln -s /usr/local/mysql/lib/libmysqlclient.18.dylib /usr/local/lib/libmysqlclient.18.dylib
 echo
